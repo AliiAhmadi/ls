74ae70d141dbc631ead7fc085f0bdb160c13c07f
AliiAhmadi <aliahmadi.github@gmail.com>
2024-03-03 21:25:57 +0330

    + ext - min_size - max_size - verbose mode - list mode

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 type App struct {
+	config Config
 }
 
 type Config struct {
+	// extension of files want to filter
+	ext string
+
+	// Minimum file size in bytes
+	min_size int64
+
+	// Maximum file size in bytes
+	max_size int64
+
+	// verbose mode
+	verbose bool
+
+	// list mode
+	list bool
 }
 
 func main() {
+
+}
+
+func (app *App) Parse() {
 
 }
